d4b5886a3630d3da2aa8e116fc056927a8473069
it24100733 <it24100733@my.sliit.lk>
2024-10-03 21:22:33 +0530

    Q2C

---
+++ Q2/IT24100733Lab7Q2C.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+
+class IT24100733lab7Q2C {
+    public static void main(String[] args) {
+
+
+
+        for (int i = 5; i >0 ; i--) {
+
+            for (int j = i; j >0 ; j--) {
+                System.out.print(i+"  ");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+}
